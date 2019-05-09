0d44d4c1e0daaa13909acad1138be02725b464f2
paperplanet <zyn2122@gmail.com>
2019-05-09 15:45:57 +0800

    add xnli_baidu dataset

--- run_classifier.py
+++ run_classifier.py
@@ -25,6 +25,9 @@ import modeling
 import optimization
 import tokenization
 import tensorflow as tf
+import setproctitle
+
+setproctitle.setproctitle("python run.py")
 
 flags = tf.flags
 
@@ -243,6 +246,50 @@ class XnliProcessor(DataProcessor):
       text_a = tokenization.convert_to_unicode(line[6])
       text_b = tokenization.convert_to_unicode(line[7])
       label = tokenization.convert_to_unicode(line[1])
+      examples.append(
+          InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+    return examples
+
+  def get_labels(self):
+    """See base class."""
+    return ["contradiction", "entailment", "neutral"]
+
+
+class XnliBaiduProcessor(DataProcessor):
+  """Processor for the XNLI data set."""
+
+  def __init__(self):
+    pass
+
+  def get_train_examples(self, data_dir):
+    """See base class."""
+    lines = self._read_tsv(
+        os.path.join(data_dir, "train.tsv"))
+    examples = []
+    for (i, line) in enumerate(lines):
+      if i == 0:
+        continue
+      guid = "train-%d" % (i)
+      text_a = tokenization.convert_to_unicode(line[0])
+      text_b = tokenization.convert_to_unicode(line[1])
+      label = tokenization.convert_to_unicode(line[2])
+      if label == tokenization.convert_to_unicode("contradictory"):
+        label = tokenization.convert_to_unicode("contradiction")
+      examples.append(
+          InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+    return examples
+
+  def get_dev_examples(self, data_dir):
+    """See base class."""
+    lines = self._read_tsv(os.path.join(data_dir, "test.tsv"))
+    examples = []
+    for (i, line) in enumerate(lines):
+      if i == 0:
+        continue
+      guid = "dev-%d" % (i)
+      text_a = tokenization.convert_to_unicode(line[0])
+      text_b = tokenization.convert_to_unicode(line[1])
+      label = tokenization.convert_to_unicode(line[2])
       examples.append(
           InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
     return examples
@@ -788,6 +835,7 @@ def main(_):
       "mnli": MnliProcessor,
       "mrpc": MrpcProcessor,
       "xnli": XnliProcessor,
+      "xnli_baidu": XnliBaiduProcessor,
   }
 
   tokenization.validate_case_matches_checkpoint(FLAGS.do_lower_case,
