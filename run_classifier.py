bbd77f3880988f8fea7fcbc650237d702bb5b022
paperplanet <zyn2122@gmail.com>
2019-05-10 15:09:34 +0800

    add chnsenticorp

--- run_classifier.py
+++ run_classifier.py
@@ -340,6 +340,45 @@ class LcqmcProcessor(DataProcessor):
       """See base class."""
       return ["0", "1"]
 
+class ChnsenticorpProcessor(DataProcessor):
+  """Processor for the LCQMC data set."""
+
+  def __init__(self):
+      pass
+
+  def get_train_examples(self, data_dir):
+      """See base class."""
+      lines = self._read_tsv(
+          os.path.join(data_dir, "train.tsv"))
+      examples = []
+      for (i, line) in enumerate(lines):
+          if i == 0:
+              continue
+          guid = "train-%d" % (i)
+          text_a = tokenization.convert_to_unicode(line[1])
+          label = tokenization.convert_to_unicode(line[0])
+          examples.append(
+              InputExample(guid=guid, text_a=text_a, label=label))
+      return examples
+
+  def get_dev_examples(self, data_dir):
+      """See base class."""
+      lines = self._read_tsv(os.path.join(data_dir, "test.tsv"))
+      examples = []
+      for (i, line) in enumerate(lines):
+          if i == 0:
+              continue
+          guid = "dev-%d" % (i)
+          text_a = tokenization.convert_to_unicode(line[1])
+          label = tokenization.convert_to_unicode(line[0])
+          examples.append(
+              InputExample(guid=guid, text_a=text_a, label=label))
+      return examples
+
+  def get_labels(self):
+      """See base class."""
+      return ["0", "1"]
+
 
 class MnliProcessor(DataProcessor):
   """Processor for the MultiNLI data set (GLUE version)."""
@@ -879,6 +918,7 @@ def main(_):
       "xnli": XnliProcessor,
       "xnli_baidu": XnliBaiduProcessor,
       "lcqmc": LcqmcProcessor,
+      "chnsenticorp": ChnsenticorpProcessor
   }
 
   tokenization.validate_case_matches_checkpoint(FLAGS.do_lower_case,
