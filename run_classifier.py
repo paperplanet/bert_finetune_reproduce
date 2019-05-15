fe9fdd8b8093080340222d5b4517a538fc99371a
paperplanet <zyn2122@gmail.com>
2019-05-15 19:35:43 +0800

    add nlpcc_dbqa

--- run_classifier.py
+++ run_classifier.py
@@ -25,9 +25,6 @@ import modeling
 import optimization
 import tokenization
 import tensorflow as tf
-import setproctitle
-
-setproctitle.setproctitle("python")
 
 flags = tf.flags
 
@@ -379,6 +376,46 @@ class ChnsenticorpProcessor(DataProcessor):
       """See base class."""
       return ["0", "1"]
 
+class NlpccdbqaProcessor(DataProcessor):
+  """Processor for the nlpcc-dbqa data set."""
+
+  def __init__(self):
+      pass
+
+  def get_train_examples(self, data_dir):
+      """See base class."""
+      lines = self._read_tsv(
+          os.path.join(data_dir, "train.tsv"))
+      examples = []
+      for (i, line) in enumerate(lines):
+          if i == 0:
+              continue
+          guid = "train-%d" % (i)
+          text_a = tokenization.convert_to_unicode(line[1])
+          text_b = tokenization.convert_to_unicode(line[2])
+          label = tokenization.convert_to_unicode(line[3])
+          examples.append(
+              InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+      return examples
+
+  def get_dev_examples(self, data_dir):
+      """See base class."""
+      lines = self._read_tsv(os.path.join(data_dir, "dev.tsv"))
+      examples = []
+      for (i, line) in enumerate(lines):
+          if i == 0:
+              continue
+          guid = "dev-%d" % (i)
+          text_a = tokenization.convert_to_unicode(line[1])
+          text_b = tokenization.convert_to_unicode(line[2])
+          label = tokenization.convert_to_unicode(line[3])
+          examples.append(
+              InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+      return examples
+
+  def get_labels(self):
+      """See base class."""
+      return ["0", "1"]
 
 class MnliProcessor(DataProcessor):
   """Processor for the MultiNLI data set (GLUE version)."""
@@ -918,7 +955,8 @@ def main(_):
       "xnli": XnliProcessor,
       "xnli_baidu": XnliBaiduProcessor,
       "lcqmc": LcqmcProcessor,
-      "chnsenticorp": ChnsenticorpProcessor
+      "chnsenticorp": ChnsenticorpProcessor,
+      "nlpccdbqa": NlpccdbqaProcessor,
   }
 
   tokenization.validate_case_matches_checkpoint(FLAGS.do_lower_case,
